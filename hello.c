599f8abd7c0039c0938e3c46d79a60f7a99373ba
Faith <fmugure@intracitygeeks.org>
2017-10-24 04:24:32 +0000

    pset1

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) //int is to initialize. Main means this is the main function to be run. Void means there is no input to the function
+{
+
+    printf("hello, world\n");
+}
