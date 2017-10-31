1abea39fa229c35c108ffd0d87f3126fe2274f2d
Faith <fmugure@intracitygeeks.org>
2017-10-31 02:38:40 +0000

    mario complete

--- mario.c
+++ mario.c
@@ -3,59 +3,30 @@
 
 int main(void)
 {
-
     int height; do
-
     //prompt the user
-
     {
         printf("Height:\n");
         height = get_int();
     }
-    while (height < 0);
+    while (height < 0 || height > 23);
     //build the pyramid
     //first build levels
 
-
-
-
-    //NEXT IS THE RIGHT SIDE OF THE PYRAMID
-printf("  \t");
+    //NEXT IS THE LEFT SIDE OF THE PYRAMID
+//printf("  \t");
     for (int height_counter = 0; height_counter < height; height_counter++)
         {
         //to build the lines
-        int spaces = height - height_counter+0; //CHANGED HEIGHT CUONTER TO +0 INSTEAD OF +1
-            for (int spaces_counter = 0; spaces_counter < spaces; spaces_counter++)
+        int spaces = height - height_counter-1; //CHANGED HEIGHT CUONTER TO +0 INSTEAD OF +1
+            for (int spaces_counter = 0; spaces_counter < spaces; spaces_counter++) //THIS LOOP'S JOB IS TO BUILD SPACES ONLY
             {
-                printf("%s", " ");
+                printf(" "); //PERCENT S MEANS WE WILL TAKE THE VARIABLE BTW THE NEXT "" SIGNS AND INPUT WHERE PCT S IS
                 //printf(" ");
 
             }
 
-        for (int blocks_counter =0; blocks_counter < height_counter; blocks_counter++)
-            {
-            printf("%s", "$");
-            }
-        //start a new line
-            printf("\n");
-
-        }
-
-
-//NEXT IS THE LEFT SIDE OF THE PYRAMID
-
-for (int height_counter = 0; height_counter < height; height_counter++)
-        {
-        //to build the lines
-        int spaces = height - height_counter+0; //CHANGED HEIGHT CUONTER TO +0 INSTEAD OF +1
-            for (int spaces_counter = 0; spaces_counter > spaces; spaces_counter++)
-            {
-                printf("%s", " ");
-                //printf(" ");
-
-            }
-
-        for (int blocks_counter = 0; blocks_counter < height_counter; blocks_counter++)
+        for (int blocks_counter =0; blocks_counter < height_counter+2; blocks_counter++) // THIS LOOP CREATES THE # BLOCKS
             {
             printf("%s", "#");
             }
@@ -63,6 +34,29 @@ for (int height_counter = 0; height_counter < height; height_counter++)
             printf("\n");
 
         }
+
+
+//NEXT IS THE RIGHT SIDE OF THE PYRAMID
+
+// for (int height_counter = 0; height_counter < height; height_counter++)
+//         {
+//         //to build the lines
+//         int spaces = height - height_counter+0; //CHANGED HEIGHT CUONTER TO +0 INSTEAD OF +1
+//             for (int spaces_counter = 0; spaces_counter > spaces; spaces_counter++)
+//             {
+//                 printf("%s", " ");
+//                 //printf(" ");
+
+//             }
+
+//         for (int blocks_counter = 0; blocks_counter < height_counter; blocks_counter++)
+//             {
+//             printf("%s", "#");
+//             }
+//         //start a new line
+//             printf("\n");
+
+//         }
 
 
 
