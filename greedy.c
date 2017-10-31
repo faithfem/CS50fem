13d7d4c2cbf44e5e2172eb54ad3dd513b149f187
Faith <fmugure@intracitygeeks.org>
2017-10-31 03:51:20 +0000

    lessgreedy

--- greedy.c
+++ greedy.c
@@ -3,9 +3,39 @@
 
 int main(void)
 {
+//int coins = 0;
+//int change = 0;
+int given_amount = 0;
+int quarters_due=0;
+int dimes_due=0;
+int nickels_due=0;
+int cents_due=0;
+int change=0;
 
 
+do
+    //prompt the user
+    {
+        printf("How much change is owed?:\n");
+        given_amount = get_int();
+    }
+    while (given_amount < 0);
 
+   // CONVERT GIVEN AMOUNT TO CENTS
+    cents_due = (int)(given_amount * 100);
 
+    //QUARTERS
+    quarters_due = (cents_due/0.25);
+
+    //DIMES
+    dimes_due = (given_amount-quarters_due);
+
+    //NICKELS
+    nickels_due =(given_amount - quarters_due - dimes_due);
+
+    //CENTS
+    cents_due = (given_amount - quarters_due - dimes_due - nickels_due);
+
+        printf("%d\n", change);
 
 }
