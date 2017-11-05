0ab568dc65660a63b77d1256e0796af882865e2e
Faith <fmugure@intracitygeeks.org>
2017-11-05 21:18:07 +0000

    water update

--- water.c
+++ water.c
@@ -1,16 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) //int is to initialize. Main means this is the main function to be run. Void means there is no input to the function
+int main(void) // Int is to initialize. Main means this is the main function to be run. Void means there is no input to the function
 {
     int minutes;
-
     do
     {
-    printf("Minutes:");
-    minutes=get_int();
-}
-    while (minutes<0);
-    printf("Bottles: %i\n", minutes*12);
-
+        printf("Minutes:");
+        minutes = get_int(); // Prompts the user to provide the minutes
+    }
+    while (minutes < 0); // While loop
+    printf("Bottles: %i\n", minutes * 12); // Number of bottles produced in a certain number of minutes
 }
